989dbf856d533bbfa2f5cf7087ffd13cca9eb434
lucca <lucca.kaasgaard@gmail.com>
2024-03-09 18:07:40 +0100

    make mediaplayer module more generic

--- .config/waybar/mediaplayer.sh
+++ .config/waybar/mediaplayer.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-player_status=$(playerctl --player=spotify status 2> /dev/null)
+PLAYER="${1:-spotify}"
+
+player_status=$(playerctl --player="$PLAYER" status 2> /dev/null)
+
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+    echo "$(playerctl --player=$PLAYER metadata artist) - $(playerctl --player=$PLAYER metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+    echo " $(playerctl --player=$PLAYER metadata artist) - $(playerctl --player=$PLAYER metadata title)"
 elif [ "$player_status" = "Stopped" ] || [ "$player_status" = "No players found" ] ; then
     echo ""
 fi
