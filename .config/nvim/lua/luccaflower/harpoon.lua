d8726ffe5ed81d00946dffe02de7b53c619ef8ae
lucca <lucca.kaasgaard@gmail.com>
2024-03-16 22:12:17 +0100

    use hyper key for harpoon

--- .config/nvim/lua/luccaflower/harpoon.lua
+++ .config/nvim/lua/luccaflower/harpoon.lua
@@ -1,10 +1,10 @@
 local ui = require("harpoon.ui")
 local mark = require("harpoon.mark")
-vim.keymap.set('n', '<leader>hn', function() mark.toggle_file() end,
+vim.keymap.set('n', '<C-S-A-D-n>', function() mark.toggle_file() end,
   { desc = "Harpoon this file" })
-vim.keymap.set('n', '<leader>ht', function() ui.toggle_quick_menu() end,
+vim.keymap.set('n', '<C-S-A-D-t>', function() ui.toggle_quick_menu() end,
   { desc = "Toggle harpoon menu" })
-vim.keymap.set('n', '<leader>hh', function() ui.nav_next() end,
+vim.keymap.set('n', '<C-S-A-D-l>', function() ui.nav_next() end,
   { desc = "Navigate to next harpoon" })
-vim.keymap.set('n', '<leader>hH', function() ui.nav_prev() end,
+vim.keymap.set('n', '<C-S-A-D-h>', function() ui.nav_prev() end,
   { desc = "Navigate to previous harpoon" })
